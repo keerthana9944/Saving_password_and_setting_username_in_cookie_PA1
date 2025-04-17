bcb6310180f58de3529fb7756bf7967a8d429377
keerthana9944 <herald.keerthana@gmail.com>
2025-04-17 12:14:54 +0530

    Completed the assignment

--- script.js
+++ script.js
@@ -15,9 +15,26 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // 1. Get the value of the 'count' cookie
+  let count = parseInt(getCookie('count')) || 0;
+
+
   // 2. If the cookie exists, increment the value and update the cookie
+  if(count){
+    count = parseInt(count);
+    count++;
+    setCookie('count', count, 365);
+  }
+
+
   // 3. If the cookie does not exist, create it and set the value to 1
+  else{
+    count = 1;
+    setCookie('count', count, 365);
+
+  }
   // 4. Display the count on the webpage
+  document.getElementById('displayCount').textContent = count;
+
 
   // your code here
 });
